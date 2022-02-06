af7b30f005a754d590e8150a4f1e5557dd802306
PaoloMenard <93705201+Yno0x@users.noreply.github.com>
2022-02-06 15:27:54 +0100

    Add files via upload

---
+++ assets/tuile.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tuile" tilewidth="30" tileheight="30" tilecount="2304" columns="64">
+ <image source="tuile.png" width="1920" height="1080"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="6">
+   <object id="6" x="-0.0597854" y="-0.0763889" width="30.1143" height="30.1061"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="400"/>
+   <frame tileid="1" duration="400"/>
+   <frame tileid="2" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="64">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0507813" y="-0.0507813" width="30.1328" height="30.1094"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="64" duration="400"/>
+   <frame tileid="65" duration="400"/>
+   <frame tileid="66" duration="400"/>
+   <frame tileid="67" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="128">
+  <animation>
+   <frame tileid="128" duration="400"/>
+   <frame tileid="129" duration="400"/>
+   <frame tileid="130" duration="400"/>
+   <frame tileid="131" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="192">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0680556" y="-0.0791667" width="30.1361" height="30.1361"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="192" duration="400"/>
+   <frame tileid="193" duration="400"/>
+   <frame tileid="194" duration="400"/>
+   <frame tileid="195" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
